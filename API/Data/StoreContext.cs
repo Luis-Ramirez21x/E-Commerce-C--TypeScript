c831a707a8978ab7728a035f3db9976d683aa398
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-03-23 18:52:00 -0400

    used dotnet ef to create a migration and create a proudcts table based off of the productr class created earlier

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
+    //derive StoreContext from DbContext
     public class StoreContext : DbContext
     {
         public StoreContext(DbContextOptions options) : base(options)
